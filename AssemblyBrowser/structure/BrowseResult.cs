ef0b4b107c11e824f6b67097d58b8f4a1af3d9cc
BlashkoHenadzi <47740678+BlashkoHenadzi@users.noreply.github.com>
2019-10-29 19:59:02 +0300

    NamespaceNode, Browse result classes complete

---
+++ AssemblyBrowser/structure/BrowseResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.structure
+{
+    class BrowseResult
+    {
+        private List<NamespaceNode> Namespaces { get; set; }
+
+        public BrowseResult(List<NamespaceNode> namespaces)
+        {
+            Namespaces = namespaces;
+        }
+        
+    }
+}
---
+++ AssemblyBrowser/structure/NamespaceNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.structure
+{
+    class NamespaceNode : INode
+    {
+        public string Name { get; set; }
+        public List<TypeNode> TypeNodes { get; set; }
+
+        public NamespaceNode(string name)
+        {
+            Name = name;
+            TypeNodes = new List<TypeNode>();
+        }
+    }
+}
